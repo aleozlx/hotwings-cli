2eb9e99fee2dc4c38f0dee617ea37ada46e31d28
Alex Yang <aleozlx@gmail.com>
2019-03-31 13:40:32 -0500

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@subcommand init =>
+            (about: "Creates a new hotwings job specification YAML (aka playbook)")
+        )
+    ).get_matches();
 }
